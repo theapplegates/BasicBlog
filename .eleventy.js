cd65be7f0a0209e82b82c05d55b4bf114003d63f
Paul Applegate <paul@theapplegates.com>
2020-03-09 13:06:07 -0400

    added stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
+const ampPlugin = require('@ampproject/eleventy-plugin-amp');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
       markdownIt = require('markdown-it');
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(ampPlugin);
   eleventyConfig.addPassthroughCopy('favicon.ico');
   const options = {
     html: true,
